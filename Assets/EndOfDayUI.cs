3248e4899cb612aefe622415635de8b52ecca6ed
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-01 19:20:16 -0300

    Finish Day Cycle

--- Assets/EndOfDayUI.cs
+++ Assets/EndOfDayUI.cs
@@ -6,11 +6,30 @@ public class EndOfDayUI : MonoBehaviour
 {
     [SerializeField] private InventoryControllerSO Inventory = null;
     [SerializeField] private TMPro.TMP_Text endDayText = null;
+    [SerializeField] private GameObject HUD = null;
+    [SerializeField] private DayCycleController dayController = null;
 
 
     public void UpdateEndOfDayText()
     {
+        HUD.SetActive(false);
         float nWorkers = FindObjectsOfType<Worker>().Length;
-        endDayText.text = $"Fim do dia, o salário será dividido entre os sócios:\n- Dinheiro restante: {Inventory.Money.ToString("{0:0.00}")} $\n-Número de sócios: {nWorkers}\n- Salário por sócio: {(Inventory.Money / nWorkers).ToString("{0:0.00}")} $";
+
+        if (nWorkers == 0)
+        {
+            endDayText.text = $"Fim do dia, você é o único sócio. Os lucros de {Inventory.Money} são seus.";
+        }
+        else
+        {
+            endDayText.text = $"Fim do dia, o salário será dividido entre os sócios:\n- Dinheiro restante: {Inventory.Money} $\n-Número de sócios: {nWorkers}\n- Salário por sócio: {(Inventory.Money / nWorkers).ToString("0.00")} $";
+        }
+    }
+
+    public void StartNextDay()
+    {
+        Inventory.ResetMoney();
+        HUD.SetActive(true);
+        dayController.ResetInitTime();
+        gameObject.SetActive(false);
     }
 }
