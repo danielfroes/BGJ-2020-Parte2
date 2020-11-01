c802512cc212547189c212b9cb8847b4efbefc7e
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-01 18:46:53 -0300

    End Of Day Cycle

---
+++ Assets/EndOfDayUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfDayUI : MonoBehaviour
+{
+    [SerializeField] private InventoryControllerSO Inventory = null;
+    [SerializeField] private TMPro.TMP_Text endDayText = null;
+
+
+    public void UpdateEndOfDayText()
+    {
+        float nWorkers = FindObjectsOfType<Worker>().Length;
+        endDayText.text = $"Fim do dia, o salário será dividido entre os sócios:\n- Dinheiro restante: {Inventory.Money.ToString("{0:0.00}")} $\n-Número de sócios: {nWorkers}\n- Salário por sócio: {(Inventory.Money / nWorkers).ToString("{0:0.00}")} $";
+    }
+}
---
+++ Assets/Scripts/DayCycleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DayCycleController : MonoBehaviour
+{
+    [Range(0, 20)]
+    [Tooltip("Tempo de um ciclo de dia em minutos")]
+    public float CycleTime = 2;
+
+    public UnityEvent OnCycleEnd = new UnityEvent();
+
+    [SerializeField] private GarbageGenerator GarbageGenerator = null;
+
+    private float _initTime = 0f;
+    private bool _isRunning = false;
+
+    [ContextMenu("Start Cycle")]
+    public void ResetInitTime()
+    {
+        _initTime = Time.time;
+        _isRunning = true;
+        GarbageGenerator.enabled = true;
+    }
+
+    private void Start()
+    {
+        ResetInitTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( CheckTime() && _isRunning)
+        {
+            GarbageGenerator.enabled = false;
+            _isRunning = false;
+            OnCycleEnd?.Invoke();
+            //ResetInitTime();
+        }
+    }
+
+    private bool CheckTime()
+    {
+        return (Time.time - _initTime) >= (CycleTime * 60f);
+    }
+}
